a5595136cb8c0ffd5887b4b75f5c009d942db28f
tika kartika <tikaputri469@gmail.com>
2026-04-13 15:25:58 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,205 +1,122 @@
 import 'package:flutter/material.dart';
 
-class MessageSearchScreen extends StatelessWidget {
-  const MessageSearchScreen({super.key});
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        centerTitle: false,
-        elevation: 0,
-        backgroundColor: const Color(0xFF00BF6D),
-        foregroundColor: Colors.white,
-        title: const Text("Chats"),
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          // Appbar search
-          Container(
-            padding: const EdgeInsets.fromLTRB(
-              16.0,
-              0,
-              16.0,
-              16.0,
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: .center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-            color: const Color(0xFF00BF6D),
-            child: Form(
-              child: TextFormField(
-                autofocus: true,
-                textInputAction: TextInputAction.search,
-                onChanged: (value) {
-                  // search
-                },
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: const Color(0xFF1D1D35).withOpacity(0.64),
-                  ),
-                  hintText: "Search",
-                  hintStyle: TextStyle(
-                    color: const Color(0xFF1D1D35).withOpacity(0.64),
-                  ),
-                  filled: true,
-                  contentPadding: const EdgeInsets.symmetric(
-                      horizontal: 16.0 * 1.5, vertical: 16.0),
-                  border: const OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const Expanded(
-            child: SingleChildScrollView(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Column(
-                children: [
-                  RecentSearchContacts(),
-                  SizedBox(height: 16.0),
-                  // you can show suggested style for search result
-                  SuggestedContacts()
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SuggestedContacts extends StatelessWidget {
-  const SuggestedContacts({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text(
-            "Suggested",
-            style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                  color: Theme.of(context)
-                      .textTheme
-                      .titleSmall!
-                      .color!
-                      .withOpacity(0.32),
-                ),
-          ),
-        ),
-        const SizedBox(height: 16.0),
-        ...List.generate(
-          demoContactsImage.length,
-          (index) => ListTile(
-            contentPadding: const EdgeInsets.symmetric(
-                horizontal: 16.0, vertical: 16.0 / 2),
-            leading: CircleAvatar(
-              radius: 24,
-              backgroundImage: NetworkImage(demoContactsImage[index]),
-            ),
-            title: const Text("Jenny Wilson"),
-            onTap: () {},
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class RecentSearchContacts extends StatelessWidget {
-  const RecentSearchContacts({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Recent search",
-            style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                  color: Theme.of(context)
-                      .textTheme
-                      .titleSmall!
-                      .color!
-                      .withOpacity(0.32),
-                ),
-          ),
-          const SizedBox(height: 16.0),
-          SizedBox(
-            width: double.infinity,
-            height: 56,
-            child: Stack(
-              children: [
-                ...List.generate(
-                  demoContactsImage.length + 1,
-                  (index) => Positioned(
-                    left: index * 48,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            width: 4,
-                            color: Theme.of(context).scaffoldBackgroundColor),
-                        shape: BoxShape.circle,
-                      ),
-                      child: index < demoContactsImage.length
-                          ? CircleAvatar(
-                              radius: 26,
-                              backgroundImage:
-                                  NetworkImage(demoContactsImage[index]),
-                            )
-                          : const RoundedCounter(total: 35),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class RoundedCounter extends StatelessWidget {
-  final int total;
-
-  const RoundedCounter({super.key, required this.total});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 52,
-      width: 52,
-      decoration: BoxDecoration(
-        color: Theme.of(context).brightness == Brightness.dark
-            ? const Color(0xFF2E2F45)
-            : const Color(0xFFEBFAF3),
-        shape: BoxShape.circle,
-      ),
-      child: Center(
-        child: Text(
-          "$total+",
-          style: Theme.of(context).textTheme.titleMedium,
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
-
-final List<String> demoContactsImage = [
-  'https://i.postimg.cc/g25VYN7X/user-1.png',
-  'https://i.postimg.cc/cCsYDjvj/user-2.png',
-  'https://i.postimg.cc/sXC5W1s3/user-3.png',
-  'https://i.postimg.cc/4dvVQZxV/user-4.png',
-  'https://i.postimg.cc/FzDSwZcK/user-5.png',
-];
